20e5e9344695acd80c582fa3249fcaa8701e0b69
Dzhambulat <dzkhasayev@gmail.com>
2018-12-28 18:50:47 +0300

    add orderbook to exchange server

--- exchange_server/app/exchange/index.js
+++ exchange_server/app/exchange/index.js
@@ -1,9 +1,13 @@
 import io from 'socket.io'
+import {OrderBook} from '../orderbook';
 
 class Exchange {
 
   constructor() {
     this.orderMap = new Map();
+    this.symOrderMap = new Map();
+    this.orderBook = new OrderBook();
+    
     this.orderMap.set('eth-eos',['tes','dsdds']);
   }
 
@@ -12,15 +16,39 @@ class Exchange {
     console.log('Listen port 5000');
     server.sockets.on('connection', (socket) => {
       socket.on('GET_LIST', (msg, callback) => {
-            console.log(msg);
-            const orders = this.orderMap.get(msg.pair);
-            console.log(callback);
-            callback(orders);
-        });
+        console.log(msg);
+        const orders = this.orderMap.get(msg.pair);
+        console.log(callback);
+        callback(orders);
+      });
+      socket.on('SELL_ORDER', (msg, callback) => {
+        this.putSellOrder(msg)
+      });
+      socket.on('BUY_ORDER', (msg, callback) => {
+        this.putBuyOrder(msg);
+      })
       socket.on('disconnect', function() {
 
       });
     });
+  }
+
+  putBuyOrder({firstSymbol, secondSymbol, amount, price}) {
+    this.orderBook.putBuyOrder({
+      price,
+      count: amount
+    })
+  }
+
+  putSellOrder({firstSymbol, secondSymbol, amount, price}) {
+    this.orderBook.putSellOrder({
+      price,
+      count: amount
+    })
+  }
+
+  putOrder({firstSymbol, secondSymbol, firstSymbolAmount, secondSymbolAmount}) {
+
   }
 } 
 
---
+++ exchange_server/app/orderbook/index.js
@@ -0,0 +1,89 @@
+import PriorityQueue from 'priorityqueue';
+import EventEmmiter from 'events';
+
+export class OrderError
+{
+
+}
+
+export class OrderBook extends EventEmmiter
+{
+    
+    constructor() {
+        super();
+        console.log("Orders");
+        this.firstSymCup = PriorityQueue({comparator: (a, b)=>
+            b.price - a.price 
+        });
+        this.secondSymCup = PriorityQueue({comparator: (a, b)=>
+            a.price - b.price 
+        });
+    }
+
+    checkOrder(order) {
+        return true;
+    }
+
+    makeExchangeResult(count,price)
+    {
+        return {
+            count:count,
+            price:price
+        }
+    }
+
+    makeExchange()
+    {
+        let buyOrder = this.buy_orders.top();
+        let sellOrder = this.sell_orders.top();
+
+        const exchangeResult = [];
+        console.log(buyOrder.price);
+        while (buyOrder && sellOrder && buyOrder.price >= sellOrder.price)
+        {
+            if (buyOrder.count <= sellOrder.count)
+            {
+                exchangeResult.push(this.makeExchangeResult(buyOrder.count, sellOrder.price))
+                this.buy_orders.pop();
+                sellOrder.count -=buyOrder.count;
+                if (sellOrder.count === 0)
+                {
+                    this.sell_orders.pop();
+                }
+            }
+            else 
+            {
+                exchangeResult.push(this.makeExchangeResult(sellOrder.count, sellOrder.price));
+                buyOrder.count -= sellOrder.count;
+                this.sell_orders.pop();
+            }
+            buyOrder = this.buy_orders.top();
+            sellOrder = this.sell_orders.top();
+        }
+
+        return exchangeResult;
+    }
+
+    putSellOrder(order){
+        if (!this.checkOrder(order))
+        {
+            throw new OrderError();
+        }
+        this.sell_orders.push(order);
+        process.nextTick(function (data) {
+            this.emit('processed',data);
+        }.bind(this) , this.makeExchange());
+    }
+
+    putBuyOrder(order) {
+        if (!this.checkOrder(order))
+        {
+            throw new OrderError();
+        }
+        this.buy_orders.push(order);
+        process.nextTick(function (data) {
+            this.emit('processed',data);
+        }.bind(this) , this.makeExchange());
+
+    }
+}
